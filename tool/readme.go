604b4cb59e269a3f61d0a03c760ad836b744e2b2
Takuto Nagami <logica0419@gmail.com>
2025-05-06 01:09:21 +0900

    add diff generation tool

---
+++ tool/readme.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/pkg/errors"
+)
+
+func copyReadMe() error {
+	log.Println("copying README.md from docs/en/index.md")
+
+	src, err := os.ReadFile("docs/en/index.md")
+	if err != nil {
+		return errors.WithStack(err)
+	}
+
+	const permission = 0o644
+	if err = os.WriteFile("README.md", src, permission); err != nil {
+		return errors.WithStack(err)
+	}
+
+	return nil
+}
